7d5d1a201253fe6dd085e2a5fd3b0ba6ee818fa0
GlennChia <glennchia7@gmail.com>
2021-08-08 14:49:41 +0800

    feat: add post request for genetic algo

--- src/app.py
+++ src/app.py
@@ -7,7 +7,25 @@ api = Api(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument('numColors', type=int, help='numer of colors in the pool')
-parser.add_argument('target', type=int, action='split', help='target')
+parser.add_argument('target', type=str, action='split', help='target')
+
+INDEX_COLORS = {
+  0: "red",
+  1: "blue",
+  2: "green",
+  3: "purple",
+  4: "yellow",
+  5: "orange",
+}
+
+COLORS_INDEX = {
+  "red": 0,
+  "blue": 1 ,
+  "green": 2,
+  "purple": 3,
+  "yellow": 4,
+  "orange": 5,
+}
 
 @api.route('/hello')
 class HelloWorld(Resource):
@@ -19,8 +37,10 @@ class Ga(Resource):
   @api.expect(parser)
   def post(self):
     args = parser.parse_args()
-    print(args['target'])
-    return {'hello': 'world'}
+    numColors, target = args['numColors'], args['target']
+    answer_num = [COLORS_INDEX.get(color) for color in target]
+    board, state = start(answer_num, False, numColors)
+    return {'board': board, 'state': state}
 
 if __name__ == '__main__':
   app.run(debug=True)
